b8042c48492b1e7b628d2ffc988f461111e8a608
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-09-25 18:33:31 -0700

    Update Max-length-difference.js

--- Max-length-difference.js
+++ Max-length-difference.js
@@ -27,3 +27,11 @@ function mxdiflg(a1, a2) {
 }
 
 // or
+
+function mxdiflg(a1, a2) {
+  const minmax = (minmax, x) => [Math.min(minmax[0], x.length), Math.max(minmax[1], x.length)];
+  if (a1.length == 0 || a2.length == 0) return -1;
+  let minmax1 = a1.reduce(minmax, [Infinity, -1]);
+  let minmax2 = a2.reduce(minmax, [Infinity, -1]);
+  return Math.max(minmax1[1] - minmax2[0], minmax2[1] - minmax1[0]);
+}
